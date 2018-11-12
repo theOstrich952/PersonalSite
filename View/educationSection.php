978e78cd8946a5ac46174f6f645bfd8c9a5e289f
theOstrich952 <D00156078@student.dkit.ie>
2018-11-12 21:51:20 +0000

    Created education section outlining education from both college and online courses

---
+++ View/educationSection.php
@@ -0,0 +1,29 @@
+<!--
+  Created by PhpStorm.
+  User: Tom Mennis
+  Date: 12/11/2018
+  Time: 20:09
+-->
+<div class="section green-section">
+  <div class="section-content">
+      <div class="title">
+          <h1>Education</h1>
+          <img src="images/section/educationIcon.png" alt="experience icon">
+      </div>
+      <div class="content">
+          <div>
+              <div class="small">2018 (Expected)</div>
+              <h2>Certificate of Completion</h2>
+              <p>The Complete React Native and Redux Course on Udemy.com</p>
+              <br><br>
+              <div class="small">2018 (Expected)</div>
+              <h2>Certificate of Completion</h2>
+              <p>C# Basics for Beginners: Learn C# Fundamentals by Coding on Udemy.com</p>
+              <br><br>
+              <div class="small">2017</div>
+              <h2>Bachelor of Science (Honours) in Computing</h2>
+              <p>Dundalk Institute of Technology</p>
+          </div>
+      </div>
+  </div>
+</div>
