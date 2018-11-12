8fcdf017ea1ec7eb8ca5ea929d75107defc94191
theOstrich952 <D00156078@student.dkit.ie>
2018-11-12 20:49:00 +0000

    Created site folder and index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/maincss.css" rel="stylesheet">
+    <link href="css/skillset.css" rel="stylesheet">
+    <link href="css/education.css" rel="stylesheet">
+    <link href="css/timeline.css" rel="stylesheet">
+    <link rel="shortcut icon" href="images/Logo/favicon.ico">
+    <title>Tom Mennis - Software Developer</title>
+</head>
+<body>
+<!--SOCIALS-->
+
+<!--HEADER-->
+
+<!--ABOUT SECTION-->
+
+<!--EDUCATION SECTION-->
+
+<!--SKILLSET SECTION-->
+
+<!--MY WORK SECTION-->
+
+</body>
+</html>
