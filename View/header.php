90dd8fa3371f1821e9a6fc3733be37e24af2b682
theOstrich952 <D00156078@student.dkit.ie>
2018-11-13 22:56:35 +0000

    Created the header of the site with name and title

---
+++ View/header.php
@@ -0,0 +1,17 @@
+<!--
+  Created by PhpStorm.
+  User: Tom Mennis
+  Date: 12/11/2018
+  Time: 20:46
+/-->
+<div class="header">
+    <div class="nav">
+        <h2>Tom Mennis</h2>
+    </div>
+    <div class="section-content">
+        <div class="section">
+            <h1>Graduate Software Developer</h1>
+            <p>A recent graduate from Dundalk Institute of Technology looking for new and exciting opportunities</p>
+        </div>
+    </div>
+</div>
