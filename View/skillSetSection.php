bd3cab60e4d0a8cc53493cf82e49838578d9980d
theOstrich952 <D00156078@student.dkit.ie>
2018-11-13 23:18:15 +0000

    Created the skill set section with all progress bars of both technical and soft skills

---
+++ View/skillSetSection.php
@@ -0,0 +1,123 @@
+<!--
+  Created by PhpStorm.
+  User: Tom Mennis
+  Date: 12/11/2018
+  Time: 20:43
+/-->
+<div class="section white-section">
+    <div class="section-content">
+        <div class="title">
+            <h1>Skill Set</h1>
+            <img src="images/section/skillSetIcon.png" alt="skill set icon">
+        </div>
+        <div class="content">
+            <div>
+                <h2>Technical Skills</h2>
+                <div class="double-col">
+                    <h2>Java</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:60%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Android Development</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:70%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Php</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:85%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>JavaScript</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:65%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>HTML 5</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:95%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>CSS 3</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:75%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>MySql</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:45%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>React Native (In The Process of Learning)</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:15%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>C# (In The Process of Learning)</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:15%"></div>
+                    </div>
+                </div>
+                <!--SOFT SKILLS-->
+                <h2 class="break">Soft Skills</h2>
+                <div class="double-col">
+                    <h2>Team Work</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:80%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Communication</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:65%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Time Keeping</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:60%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Prioritization of Work</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:75%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Working Under Pressure</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:90%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Willingness to Learn</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:95%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Adaptability</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:55%"></div>
+                    </div>
+                </div>
+                <div class="double-col">
+                    <h2>Critical Thinking</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width:70%"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
