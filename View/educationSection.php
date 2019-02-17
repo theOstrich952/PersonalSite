791433c5f3c816b8556e4d86acf98257d17f568e
theOstrich952 <D00156078@student.dkit.ie>
2019-02-17 13:16:48 +0000

    Changed css styling of section

--- View/educationSection.php
+++ View/educationSection.php
@@ -4,19 +4,19 @@
   Date: 12/11/2018
   Time: 20:09
 -->
-<div class="section green-section">
+<div class="section blue-section">
   <div class="section-content">
       <div class="title">
           <h1>Education</h1>
-          <img src="images/section/educationIcon.png" alt="experience icon">
+          <img src="View/images/section/educationIcon.png" alt="experience icon">
       </div>
       <div class="content">
           <div>
-              <div class="small">2018 (Expected)</div>
+              <div class="small">2019 (Expected)</div>
               <h2>Certificate of Completion</h2>
               <p>The Complete React Native and Redux Course on Udemy.com</p>
               <br><br>
-              <div class="small">2018 (Expected)</div>
+              <div class="small">2019 (Expected)</div>
               <h2>Certificate of Completion</h2>
               <p>C# Basics for Beginners: Learn C# Fundamentals by Coding on Udemy.com</p>
               <br><br>
