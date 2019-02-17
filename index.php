b5ae7045c2992520f8bcc6c81077e68556a9e5ae
theOstrich952 <D00156078@student.dkit.ie>
2019-02-17 13:54:35 +0000

    Added includes for about, education, skillset and work sections

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="css/maincss.css" rel="stylesheet">
-    <link href="css/header.css" rel="stylesheet">
-    <link href="css/socials.css" rel="stylesheet">
-    <link href="css/skillset.css" rel="stylesheet">
-    <link href="css/education.css" rel="stylesheet">
-    <link href="css/timeline.css" rel="stylesheet">
-    <link rel="shortcut icon" href="images/Logo/favicon.ico">
+    <link href="View/css/maincss.css" rel="stylesheet">
+    <link href="View/css/header.css" rel="stylesheet">
+    <link href="View/css/socials.css" rel="stylesheet">
+    <link href="View/css/skillset.css" rel="stylesheet">
+    <link href="View/css/education.css" rel="stylesheet">
+    <link href="View/css/work.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="View/images/Logo/favicon.ico">
     <title>Tom Mennis - Software Developer</title>
 </head>
 <body>
@@ -18,12 +18,13 @@ include "View/socialSection.php";
 /*HEADER*/
 include "View/header.php";
 /*ABOUT SECTION*/
-
+include "View/aboutSection.php";
 /*EDUCATION SECTION*/
-
-/*SKILLSET SECTION*/
-
+include "View/educationSection.php";
 /*MY WORK SECTION*/
+include "View/myWorkSection.php";
+/*SKILLSET SECTION*/
+include "VIEW/skillSetSection.php";
 ?>
 </body>
 </html>
