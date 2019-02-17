ae4247af2549fc7fcc967368c817e84bdd913359
theOstrich952 <D00156078@student.dkit.ie>
2019-02-17 13:14:55 +0000

    Modified the about section text

--- View/aboutSection.php
+++ View/aboutSection.php
@@ -8,14 +8,15 @@
     <div class="section-content">
         <div class="title">
             <h1>ABOUT</h1>
-            <img src="images/section/aboutIcon.png" alt="about icon">
+            <img src="View/images/section/aboutIcon.png" alt="about icon">
         </div>
         <div class="content">
             <div>
                 <h2>Tom Mennis</h2>
                 <p>I graduated Dundalk Institute of Technology in November 2017 with a 2.1 Bachelor's Degree </p><br>
                 <p>I am an ambitious, curious and passionate developer with a keen eye for design and detail. Currently I write in Java both through JVM and Android, JavaScript, PHP 5 & 7
-                    , HTML 5, CSS3 I also know small bits of both Haskell and Prolog.</p>
+                    , HTML 5, CSS3 I also know small bits of both Haskell and Prolog.</p><br>
+                <p>Currently I am Upskilling my Java and Learning Java Spring, C# and React Native all through Udemy.com</p>
             </div>
         </div>
     </div>
