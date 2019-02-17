6ff1660470eb709027a61f76230b3cce2dce0c77
theOstrich952 <D00156078@student.dkit.ie>
2019-02-17 13:53:32 +0000

    Image directories changed

--- View/socialSection.php
+++ View/socialSection.php
@@ -5,7 +5,7 @@
   Time: 20:47
 -->
 <div class="socials">
-    <a href="https://github.com/theOstrich952" target="_blank"><img src="images/social/gitIcon.png"></a>
-    <a href="https://www.linkedin.com/in/tom-mennis-3a5438132/" target="_blank"><img src="images/social/linkedInIcon.png"></a>
-    <a href="https://drive.google.com/open?id=1a1z-ScA1SV_DjRk9W26wCxdrF1kcNEX2" target="_blank"><img src="images/social/cvIcon.png"></a>
+    <a href="https://github.com/theOstrich952" target="_blank"><img src="View/images/social/gitIcon.png"></a>
+    <a href="https://www.linkedin.com/in/tom-mennis-3a5438132/" target="_blank"><img src="View/images/social/linkedInIcon.png"></a>
+    <a href="https://drive.google.com/open?id=1a1z-ScA1SV_DjRk9W26wCxdrF1kcNEX2" target="_blank"><img src="View/images/social/cvIcon.png"></a>
 </div>
