ade947716cc4a9ceeb6be4533d4a8788b1c14251
theOstrich952 <D00156078@student.dkit.ie>
2019-02-17 13:25:27 +0000

    Changed css styling

--- View/header.php
+++ View/header.php
@@ -4,14 +4,14 @@
   Date: 12/11/2018
   Time: 20:46
 /-->
-<div class="header">
+<div class="header blue-section">
     <div class="nav">
         <h2>Tom Mennis</h2>
     </div>
     <div class="section-content">
         <div class="section">
             <h1>Graduate Software Developer</h1>
-            <p>A recent graduate from Dundalk Institute of Technology looking for new and exciting opportunities</p>
+            <p>A graduate from Dundalk Institute of Technology looking for new and exciting opportunities within the IT sector</p>
         </div>
     </div>
 </div>
