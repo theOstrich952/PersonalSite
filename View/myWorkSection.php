6a7482c5f47080e7d8dddbace463eff218e5c0fe
theOstrich952 <D00156078@student.dkit.ie>
2019-02-17 12:45:10 +0000

    Created the mork section to contain all work done to date

---
+++ View/myWorkSection.php
@@ -0,0 +1,59 @@
+<!--
+  Created by PhpStorm.
+  User: Tom Mennis
+  Date: 12/11/2018
+  Time: 20:45
+-->
+<div class="section white-section">
+    <div class="section-content">
+        <div class="title">
+            <h1>My Work</h1>
+            <img src="View/images/section/workIcon.png" alt="experience icon">
+        </div>
+        <div class="content">
+            <div>
+                <h2>Websites Created</h2>
+                <div class="double-col img-col">
+                    <a href="https://farmazon.ie" target="_blank">
+                        <img src="View/images/Work/Farmazon.jpg">
+                    </a>
+                    <a href="https://farmazon.ie" target="_blank">
+                        <h2>Farmazon.ie</h2>
+                    </a>
+                    <div class="tag">HTML 5</div>
+                    <div class="tag">CSS 3</div>
+                    <div class="tag">JavaScript</div>
+                    <div class="tag">PHP 7</div>
+                    <div class="tag">BootStrap</div>
+                </div>
+
+
+                <div class="double-col img-col">
+                    <a href="View/Projects/Modify/index.php" target="_blank">
+                        <img src="View/images/Work/Modify.jpg">
+                    </a>
+                    <a href="View/MyWork/Modify/index.php" target="_blank">
+                        <h2>Modify</h2>
+                    </a>
+                    <div class="tag">HTML 5</div>
+                    <div class="tag">CSS 3</div>
+                    <div class="tag">JavaScript</div>
+                    <div class="tag">PHP 5</div>
+                </div>
+
+                <div class="double-col img-col">
+                    <a href="View/Projects/RmiChatRoom/rmiChatRoom.php">
+                        <img src="View/images/Work/ChatRoom.jpg">
+                    </a>
+                    <a href="View/MyWork/RmiChatRoom/rmiChatRoom.php">
+                        <h2>RMI Chat Room</h2>
+                    </a>
+                    <div class="tag">Java</div>
+                    <div class="tag">Java FX</div>
+                    <div class="tag">SQL</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
